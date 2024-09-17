6c1398bdf8449b0dee1a008883967355cbe72dbb
Petr Pyatkov <Bigbox89@mail.ru>
2024-09-17 10:59:23 +0500

    CRMX-112 добавлены контроллер и сервисы для сервиса авторизации

--- src/main/java/com/croco/auth/service/AuthService.java
+++ src/main/java/com/croco/auth/service/AuthService.java
@@ -1,16 +1,10 @@
 package com.croco.auth.service;
 
+import com.croco.auth.dto.AuthRequestDTO;
 import com.croco.auth.dto.AuthResponseDTO;
 import com.croco.auth.dto.LoginRequest;
 import com.croco.auth.dto.UserDTO;
 
 public interface AuthService {
-
-    String login(LoginRequest request);
-    void createUser(UserDTO userDTO);
-    UserDTO getUser(Long id);
-    void updateUser(UserDTO userDTO);
-    void deleteUser(Long userId);
-    AuthResponseDTO authenticate(String subsystemName, String loginName, String hashedPassword);
-
+    AuthResponseDTO authenticate(AuthRequestDTO authRequest);
 }
